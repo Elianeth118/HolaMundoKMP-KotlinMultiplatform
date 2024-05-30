dfbdd92cfd9a458df8c40b417b53323a1e90fa55
Elianeth118 <aragonelianeth@gmail.com>
2024-05-29 20:01:42 -0600

    Subido

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,44 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import holamundokmp.composeapp.generated.resources.Res
+import holamundokmp.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+
+fun App() {
+    MaterialTheme {
+        var name: String by remember { mutableStateOf("") }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = name,
+                onValueChange = { name = it }
+            )
+            Spacer(modifier = Modifier.height(38.dp))
+            AnimatedVisibility(name.isNotEmpty()) {
+//
+                Text("Hola $name", fontSize = 24.sp)
+            }
+        }
+    }
+}
